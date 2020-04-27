ae0f72a697de7b131289cfd7975fdb774dfb258c
Abildin <Abildin@users.noreply.github.com>
2020-04-27 15:32:24 +0600

    Change requirements.txt (#3)

--- .github/scripts/check-auto-approve.sh
+++ .github/scripts/check-auto-approve.sh
@@ -2,9 +2,7 @@
 
 git fetch --no-tags --prune --depth=1 origin $BASE_REF $HEAD_REF
 
-git diff --name-only origin/$BASE_REF origin/$HEAD_REF
-
-CHANGED_FILES=
+CHANGED_FILES=$(git diff --name-only origin/$BASE_REF origin/$HEAD_REF)
 
 if [[ $CHANGED_FILES == $TARGET_FILES ]]; then
   exit 0
